43082e9fb594338176b27faff1dc55b896d8f828
Indroneel Das <neel@randondiesel.com>
2019-01-29 10:07:15 +0530

    Feature: take a path into the json data and convert it into a Java type. Feature: new annotation @Raw to handover a subset of the JSON string to a field of type byte[] or String for custom processing.

--- src/main/java/rd/jsonmapper/decode/Json2Object.java
+++ src/main/java/rd/jsonmapper/decode/Json2Object.java
@@ -20,6 +20,8 @@ import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -36,17 +38,9 @@ import rd.jsonmapper.support.JSONTokener;
 public class Json2Object {
 
 	private DecoderRegistryImpl reg;
+	private JSONTokener         tokener;
 
-	public Json2Object() {
-		reg = new DecoderRegistryImpl();
-		reg.objDecoder = new ObjectDecoder(reg);
-		reg.fldDecoder = new FieldDecoder(reg);
-		reg.listDecoder = new ListDecoder(reg);
-		reg.arrDecoder = new ArrayDecoder(reg);
-		reg.mapDecoder = new MapDecoder(reg);
-	}
-
-	public Object convert(InputStream in, Type type) throws IOException {
+	public Json2Object(InputStream in) throws IOException {
 		byte[] buffer = new byte[0];
 		byte[] chunk = new byte[1024];
 		int amt = 0;
@@ -60,14 +54,17 @@ public class Json2Object {
 			buffer = tempbuf;
 		}
 		in.close();
-		return convert(buffer, type);
+		prepare(buffer);
 	}
 
-	public Object convert(byte[] data, Type type) {
-		if(data == null) {
+	public Json2Object(byte[] data) {
+		prepare(data);
+	}
+
+	public Object convert(Type type) {
+		if(tokener == null) {
 			return null;
 		}
-		JSONTokener tokener = new JSONTokener(new ByteArrayInputStream(data));
 		Object nextVal = tokener.nextValue();
 		if(nextVal == null) {
 			return null;
@@ -108,6 +105,179 @@ public class Json2Object {
 			}
 		}
 		return null;
+	}
+
+	public Object convert(String path, Type type) {
+		if(tokener == null) {
+			return null;
+		}
+
+		Object rootVal = tokener.nextValue();
+		if(rootVal == null || !(rootVal instanceof JSONObject)) {
+			return null;
+		}
+		JSONObject nowVal = (JSONObject) rootVal;
+
+		String[] pathElements = path.split("\\.");
+		for(int i = 0; i < (pathElements.length - 1); i++) {
+			try {
+				nowVal = nowVal.getJSONObject(pathElements[i]);
+			}
+			catch(Exception exep) {
+				return null;
+			}
+		}
+		String name = pathElements[pathElements.length - 1];
+
+		if(type instanceof Class) {
+			Class<?> ctype = (Class<?>) type;
+			if(ctype.equals(Boolean.TYPE) || ctype.equals(Boolean.class)) {
+				try {
+					return nowVal.getBoolean(name);
+				}
+				catch(Exception exep) {
+					return ctype.equals(Boolean.TYPE) ? false : null;
+				}
+			}
+			if(ctype.equals(Byte.TYPE) || ctype.equals(Byte.class)) {
+				try {
+					return Byte.decode(nowVal.getString(name));
+				}
+				catch(Exception exep) {
+					return ctype.equals(Byte.TYPE) ? (byte) 0 : null;
+				}
+			}
+			if(ctype.equals(Character.TYPE) || ctype.equals(Character.class)) {
+				try {
+					return nowVal.getString(name).charAt(0);
+				}
+				catch(Exception exep) {
+					return ctype.equals(Character.TYPE) ? (char) 0 : null;
+				}
+			}
+			if(ctype.equals(Short.TYPE) || ctype.equals(Short.class)) {
+				try {
+					Integer value = nowVal.getInt(name);
+					return value.shortValue();
+				}
+				catch(Exception exep) {
+					return ctype.equals(Short.TYPE) ? (short) 0 : null;
+				}
+			}
+			if(ctype.equals(Integer.TYPE) || ctype.equals(Integer.class)) {
+				try {
+					return nowVal.getInt(name);
+				}
+				catch(Exception exep) {
+					return ctype.equals(Integer.TYPE) ? 0 : null;
+				}
+			}
+			if(ctype.equals(Long.TYPE) || ctype.equals(Long.class)) {
+				try {
+					return nowVal.getLong(name);
+				}
+				catch(Exception exep) {
+					return ctype.equals(Long.TYPE) ? (long) 0 : null;
+				}
+			}
+			if(ctype.equals(Float.TYPE) || ctype.equals(Float.class)) {
+				try {
+					Double value = nowVal.getDouble(name);
+					return value.floatValue();
+				}
+				catch(Exception exep) {
+					return ctype.equals(Float.TYPE) ? (float) 0 : null;
+				}
+			}
+			if(ctype.equals(Double.TYPE) || ctype.equals(Double.class)) {
+				try {
+					return nowVal.getDouble(name);
+				}
+				catch(Exception exep) {
+					return ctype.equals(Double.TYPE) ? (double) 0 : null;
+				}
+			}
+			if(ctype.equals(BigInteger.class)) {
+				try {
+					return nowVal.getBigInteger(name);
+				}
+				catch(Exception exep) {
+					return null;
+				}
+			}
+			if(ctype.equals(BigDecimal.class)) {
+				try {
+					return nowVal.getBigDecimal(name);
+				}
+				catch(Exception exep) {
+					return null;
+				}
+			}
+			//then the rest
+			if(ctype.isArray()) {
+				try {
+					JSONArray jarr = nowVal.getJSONArray(name);
+					Object[] values = reg.arrayDecoder().convert(jarr, ctype);
+					if(values != null) {
+						Object arrVal = Array.newInstance(ctype.getComponentType(), values.length);
+						for(int i=0; i < values.length; i++) {
+							Array.set(arrVal, i, values[i]);
+						}
+						return arrVal;
+					}
+				}
+				catch(Exception exep) {}
+				return null;
+			}
+			//finally
+			try {
+				JSONObject jobj = nowVal.getJSONObject(name);
+				return reg.objectDecoder().convert(jobj, ctype);
+			}
+			catch(Exception exep) {}
+			return null;
+		}
+
+		if(type instanceof ParameterizedType) {
+			ParameterizedType ptype = (ParameterizedType) type;
+			Type rtype = ptype.getRawType();
+			try {
+				Object lastVal = nowVal.get(name);
+				if(lastVal instanceof JSONArray) {
+					if(rtype.equals(List.class)) {
+						return reg.listDecoder.convertList((JSONArray) lastVal, type);
+					}
+					if(rtype.equals(Set.class)) {
+						return reg.listDecoder.convertSet((JSONArray) lastVal, type);
+					}
+				}
+				else if(lastVal instanceof JSONObject) {
+					if(rtype.equals(Map.class)) {
+						return reg.mapDecoder.convert((JSONObject) lastVal, type);
+					}
+				}
+			}
+			catch(Exception exep) {}
+			return null;
+		}
+		return null;
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// Helper methods
+
+	private void prepare(byte[] data) {
+		reg = new DecoderRegistryImpl();
+		reg.objDecoder = new ObjectDecoder(reg);
+		reg.fldDecoder = new FieldDecoder(reg);
+		reg.listDecoder = new ListDecoder(reg);
+		reg.arrDecoder = new ArrayDecoder(reg);
+		reg.mapDecoder = new MapDecoder(reg);
+
+		if(data == null) {
+			return;
+		}
+		tokener = new JSONTokener(new ByteArrayInputStream(data));
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////
